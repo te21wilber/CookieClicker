8de12d3b829b08857664c4daaadc212c68b55060
William Bergman <te21wilber@falufri.se>
2023-03-20 16:06:31 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function () {    
+    
+}
+
+function dissmissStartPopup(){
+    var startPopup = document.getElementById("startPopup");
+    startPopup.style.display = 'none';
+    // newspaper.animate(animateclose, 1000);
+    // setTimeout(function() {
+    //     startPopup.style.display = 'none';
+    //   }, 1000);
+}
